980739dc2a9edbc8832203cfc965cead3decc302
Sukhi Grewal <sdotkr@gmail.com>
2021-10-05 16:45:04 +0000

    Initial commit

---
+++ siteConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "London After Midnight",
+  shortName: "LAM",
+  description:
+    "A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery",
+  url: "https://gatsby-lam.weensy.co", // Domain of your site without prefix!
+  prefix: "/",
+  author: "weensykim",
+  twitter: "",
+}
